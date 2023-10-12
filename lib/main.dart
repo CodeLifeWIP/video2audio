5aaec0c37c3ea6bf74398bf1b203f1d0118ada37
ebenavidez <96456755+ebenavidez@users.noreply.github.com>
2023-10-12 20:43:01 +0800

    adding firebase and crashlytics plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:video_to_audio/modules/home/presentation/bloc/downloader_bloc.dart';
@@ -7,6 +10,22 @@ import 'core/injection.dart' as di;
 
 main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+  // Pass all uncaught "fatal" errors from the framework to Crashlytics
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
+
   di.init();
   await di.locator.allReady();
   runApp(const MyApp());
